2f1f697d395410e952bb6fe75800d86b3ad3fb47
AbC <127856485+abciam@users.noreply.github.com>
2026-03-25 21:29:11 +0500

    Add files via upload

---
+++ Test/Class1.cs
@@ -0,0 +1,41 @@
+﻿using Common.Enums;
+using Common.Events;
+using Common.Interfaces;
+using Common.Models;
+
+namespace Test;
+
+public class Class1 : AsyncModule
+{
+    public readonly Guid guid = Guid.NewGuid();
+    public override ModuleIdentity Identity => new("1test", guid, "1.0f1");
+
+    public override void Render(IUiBuilder ui)
+    {
+        Bus.Publish(new ProgressEvent(Identity, 0, $"Загружено {0}%"));
+        ui.AddAction("Test", async () =>
+        {
+            Bus.Publish(new LogEvent("Start Heavy Task", ELog.Basic));
+
+            bool isStarted = await Context.Execute(ETaskWeight.Heavy, async () =>
+            {
+                for (int i = 0; i <= 100; i += 1)
+                {
+                    await Task.Delay(500);
+                    Bus.Publish(new ProgressEvent(Identity, i, $"Загружено {i}%"));
+                }
+            });
+
+            if (isStarted)
+            {
+                Bus.Publish(new LogEvent("Heavy Task Succesfuly", ELog.Basic));
+                ui.AddInfo("Heavy Task Succesfuly");
+            }
+        });
+    }
+
+    protected override async Task<bool> OnInitAsync()
+    {
+        return true;
+    }
+}
